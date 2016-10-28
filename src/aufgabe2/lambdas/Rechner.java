5e25024b9e13e0a4269fafdafb4767ebd2cf9b4b
Simon Felske <simon-frederik.felske@haw-hamburg.de>
2016-10-28 17:05:32 +0200

    z2_a1_klassen

---
+++ src/aufgabe2/lambdas/Rechner.java
@@ -0,0 +1,14 @@
+package aufgabe2.lambdas;
+
+public class Rechner {
+	
+	public enum Operation {ADDITION, SUBTRAKTION, MULTIPLIKATION, DIVISION};
+	
+	public Rechner(){
+	}
+	
+	public double berechne(Operation opcode, double zahl1, double zahl2){
+		return 1.0;
+	}
+
+}
---
+++ src/aufgabe2/lambdas/Rechnertest.java
@@ -0,0 +1,14 @@
+package aufgabe2.lambdas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Rechnertest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
