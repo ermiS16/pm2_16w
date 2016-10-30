f0f88a6bb160fe6e937862f83aadb04f27071fc5
Simon Felske <simon-frederik.felske@haw-hamburg.de>
2016-10-30 16:28:17 +0100

    testklasse, arrays in Stringverarbeitung

--- src/aufgabe2/streams/Stringverarbeitung.java
+++ src/aufgabe2/streams/Stringverarbeitung.java
@@ -2,4 +2,16 @@ package aufgabe2.streams;
 
 public class Stringverarbeitung {
 
+	String[] standardwerte = {"Eingabe ","Äußeres  ", null, "Strassen-Feger", " ein Haus"};
+	String[] nutzerwerte;
+	
+	Stringverarbeitung(){
+	}
+	
+	Stringverarbeitung (String... nutzereingabe){
+		nutzerwerte = new String[nutzereingabe.length];
+		for (int i = 0; i < nutzereingabe.length; i++){
+			nutzerwerte[i] = nutzereingabe[i];
+		}
+	}
 }
