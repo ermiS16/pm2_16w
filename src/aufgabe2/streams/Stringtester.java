32bf26674ce5bb74775e7aa5750efff2d070b6f4
Simon Felske <simon-frederik.felske@haw-hamburg.de>
2016-10-30 16:30:45 +0100

    testklasse adden vergessen

---
+++ src/aufgabe2/streams/Stringtester.java
@@ -0,0 +1,14 @@
+package aufgabe2.streams;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Stringtester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
