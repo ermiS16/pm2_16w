66dcb063da5e76bd25d19eda45f83ca9a8a8703a
Simon Felske <simon-frederik.felske@haw-hamburg.de>
2016-11-02 16:52:18 +0100

    aufgabe2_ohne8erlimit

--- src/aufgabe2/streams/Stringtester.java
+++ src/aufgabe2/streams/Stringtester.java
@@ -4,11 +4,47 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.stream.*;
+import java.util.*;
+
 public class Stringtester {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testeVerarbeite() {
+		Stringverarbeitung test1 = new Stringverarbeitung();
+		test1.verarbeite();
+		assertEquals(test1.getListe().contains("EINGABE"), true);
+		//assertEquals(test1.getListe().contains("AEUSSERE"), true);
+		//assertEquals(test1.getListe().contains("STRASSEN"), true);
+		assertEquals(test1.getListe().contains("EIN HAUS"), true);
+		assertEquals(test1.getListe().contains(" EIN HAUS"), false);
+		//System.out.println(test1.getListe());
+	}
+	
+	@Test
+	public void testeContainsNull(){
+		Stringverarbeitung test = new Stringverarbeitung();
+		test.verarbeite();
+		assertEquals(test.getListe().contains(null), false);
+		//System.out.println(test.getListe());
 	}
 
+	@Test
+	public void testeOnlyContainsNull(){
+		Stringverarbeitung test = new Stringverarbeitung(null, null, null);
+		test.verarbeite();
+		assertEquals(test.getListe().contains(null), false);
+		//System.out.println(test.getListe());
+	}
+	
+	@Test
+	public void testeSonderzeichen(){
+		Stringverarbeitung test = new Stringverarbeitung("Ä", "ä", "Ü", "ü", "Ö", "ö", "Straße", "#");
+		test.verarbeite();
+		assertEquals(test.getListe().contains("#"), true);
+		assertEquals(test.getListe().contains("ä"), false);
+		assertEquals(test.getListe().contains("UE"), true);
+		//System.out.println(test.getListe());
+	}
 }
--- src/aufgabe2/streams/Stringverarbeitung.java
+++ src/aufgabe2/streams/Stringverarbeitung.java
@@ -1,17 +1,51 @@
 package aufgabe2.streams;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class Stringverarbeitung {
 
-	String[] standardwerte = {"Eingabe ","Äußeres  ", null, "Strassen-Feger", " ein Haus"};
-	String[] nutzerwerte;
+	private String[] standardwerte = {"Eingabe ","Äußeres  ", null, "Strassen-Feger", " ein Haus"};
+	private String[] nutzerwerte;
+	private List<String> speicherliste;
 	
+	/*
+	 * Constructor
+	 */
 	Stringverarbeitung(){
 	}
 	
+	/*
+	 * Constructor mit Nutzereingabe
+	 * @param nutzereingabe - beliebig viele Strings per varargs
+	 */
 	Stringverarbeitung (String... nutzereingabe){
-		nutzerwerte = new String[nutzereingabe.length];
+		standardwerte = new String[nutzereingabe.length];
 		for (int i = 0; i < nutzereingabe.length; i++){
-			nutzerwerte[i] = nutzereingabe[i];
+			standardwerte[i] = nutzereingabe[i];
 		}
+	}
+	
+	/*
+	 * Verarbeitet die im Array gegebenen Strings 
+	 * in einem Stream nach den Vorgaben
+	 * und legt sie in einer List ab
+	 */
+	public void verarbeite(){
+		List<String> ph = Arrays.asList(standardwerte);
+		Stream<String> t1 = ph.stream();
+		nutzerwerte = t1.filter(o -> o!=null).map(e -> e.replace("ß", "ss"))
+		.map(txt -> txt.toUpperCase()).map(e -> e.replace("Ä", "AE"))
+		.map(e -> e.replace("Ö", "OE")).map(e -> e.replace("Ü", "UE"))
+		.map(e -> e.trim()).toArray(String[]::new);
+		speicherliste = Arrays.asList(nutzerwerte);
+	}
+	
+	/*
+	 * Gibt die Liste zurueck
+	 * @return - Liste mit bearbeiteten Werten
+	 */
+	public List<String> getListe(){
+		return speicherliste;
 	}
 }
