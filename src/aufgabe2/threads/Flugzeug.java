00d0e2b37cf5a03e114d7eb3d55038a626017113
Simon Felske <simon-frederik.felske@haw-hamburg.de>
2016-11-03 14:56:05 +0100

    Z2_A3_rohbau

--- src/aufgabe2/threads/Flugzeug.java
+++ src/aufgabe2/threads/Flugzeug.java
@@ -1,5 +1,42 @@
 package aufgabe2.threads;
 
 public class Flugzeug {
+	
+	private Flughafen flughafen;
+	private String id;
+	private int flugdauer;
+	private int startzeit;
+	private Status status;
+	private int zeit;
+	
+	public Flugzeug(String id, int flugdauer, Flughafen zielort, int zeit){
+		this.id = id;
+		this.flugdauer = flugdauer;
+		flughafen = zielort;
+		startzeit = zeit;
+		status = Status.IM_FLUG;
+	}
 
+	public void istGelandet(){
+		status = Status.GELANDET;
+	}
+	
+	public boolean isGelandet(){
+		boolean amBoden = false;
+		if(status == Status.GELANDET){
+			amBoden = true;
+		}
+		return amBoden;
+	}
+	
+	public String toString(){
+		String formatiert = "";
+		formatiert += "Flugzeug-ID: " + id + " Zielort: " + flughafen + " Flugdauer: " + flugdauer + " Zeit: " + startzeit + " Status: " + status;
+		return formatiert;
+	}
+	
+	public void setZeit(int zeitwert){
+		
+	}
+	
 }
